ed1b47a3966d7dfb5faa1d0354759edc39eec86b
Filippo Bellon <filippo.uni.bellon03@gmail.com>
2024-05-17 22:27:55 +0200

    Implementazione conversione e stampa dei primi 3 numeri
    Implementazione test per primi 3 numeri
    
    Implementazione errore fuori range
    
    Close #1, Close #9, Close #17;

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,8 +7,19 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
 
-    public static String convert(int number) {
-        // TODO
-        return null;
+    public static String convert(int number){
+
+        //Implementazione controllo > 0 && < 4 altrimenti return errore
+        if (number > 0 && number < 4){
+            if (number == 1){
+                return "I";
+            } else if (number == 2){
+                return "II";
+            } else {
+                return "III";
+            }
+        }
+        // Se il programma arriva qua --> non ho fatto conversioni, quindi il valore non rispetta i limiti
+        return "Errore: Numero non rispetta le PRE-Condizioni del Programma";
     }
 }
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -5,12 +5,40 @@
 
 package it.unipd.mtss;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class RomanPrinterTest {
-    
-    @Test
-    public void testPrint() {
 
+    @Test
+    public void testPrimi3() {
+        
+        //Test primi 3 numeri
+        
+        String v_1 = "  _____  \n" +
+        " |_   _| \n" +
+        "   | |   \n" +
+        "   | |   \n" +
+        "  _| |_  \n" +
+        " |_____| \n";
+
+        String v_2 = "  _____    _____  \n" + 
+        " |_   _|  |_   _| \n" + 
+        "   | |      | |   \n" +
+        "   | |      | |   \n" +  
+        "  _| |_    _| |_  \n" + 
+        " |_____|  |_____| \n";
+
+        String v_3 = "  _____    _____    _____  \n"+ 
+        " |_   _|  |_   _|  |_   _| \n" + 
+        "   | |      | |      | |   \n" +
+        "   | |      | |      | |   \n" +
+        "  _| |_    _| |_    _| |_  \n" +
+        " |_____|  |_____|  |_____| \n";
+
+        assertEquals(v_1, RomanPrinter.print(1));
+        assertEquals(v_2, RomanPrinter.print(2));
+        assertEquals(v_3, RomanPrinter.print(3));
     }
 }
